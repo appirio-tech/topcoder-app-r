9e8a64101aee88cdbcca4e5430471ae6ad7c7b2e
Nick Litwin <nlitwin@appirio.com>
2016-01-20 22:23:07 -0800

    Add linting and fix errors

--- app/index.js
+++ app/index.js
@@ -1,28 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
-import { Router, Route, Link } from 'react-router'
+import { Router, Route } from 'react-router'
 
 const App = React.createClass({
   render: function() {
     return (
-      <div>Hello, Nick</div>
+      <Search></Search>
     )
   }
 })
 
-const Home = React.createClass({
+const Search = React.createClass({
   render: function() {
     return (
-      <div>At home</div>
+      <div>Search Page</div>
     )
   }
 })
 
 render((
   <Router history={createBrowserHistory()}>
-    <Route path="/" component={App}>
-      <Route path="home" component={Home}/>
+    <Route path="/search" component={App}>
     </Route>
   </Router>
 ), document.getElementById('root'))

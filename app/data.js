ebace21bd74cf530409459f1c9e36ca683e16b2b
Nick Litwin <nlitwin@appirio.com>
2016-02-16 09:51:31 -0800

    Add fake data for fake api calls

---
+++ app/data.js
@@ -0,0 +1,37 @@
+export default {
+  'skills': [
+    {
+      'id': 1,
+      'name': 'JavaScript'
+    },
+    {
+      'id': 2,
+      'name': 'Python'
+    },
+    {
+      'id': 3,
+      'name': 'React'
+    },
+    {
+      'id': 4,
+      'name': 'Redux'
+    }
+  ],
+  'users': [
+    {
+      id: 3,
+      'username': 'nick',
+      'skills': [1, 3, 4]
+    },
+    {
+      id: 3,
+      'username': 'sheldon',
+      'skills': [1, 2, 3, 4]
+    },
+    {
+      id: 3,
+      'username': 'leonard',
+      'skills': [2]
+    }
+  ]
+}

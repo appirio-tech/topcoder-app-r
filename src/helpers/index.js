a30c728d571346eff712803882a8063591ae8f52
Nick Litwin <nlitwin@appirio.com>
2016-03-10 16:46:20 -0800

    Add helper functions for level color and subtrack abbreviations

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -13,8 +13,8 @@ export function json(response) {
   return response.json()
 }
 
-// Miscellaneous
-export function memberLevel(userRating) {
+// Member Levels
+export function memberLevelByRating(userRating) {
   const levelRatings = [0, 900, 1200, 1500, 2200]
 
   const userLevel = _.findLastIndex(levelRatings, (rating) => {
@@ -25,4 +25,50 @@ export function memberLevel(userRating) {
   })
 
   return userLevel + 1
+}
+
+export function memberColorByLevel(userLevel) {
+  const colorsByLevel = {
+    1: '#9D9FA0',
+    2: '#69C329',
+    3: '#616BD5',
+    4: '#FCD617',
+    5: '#EF3A3A'
+  }
+
+  const color = colorsByLevel[userLevel] || colorsByLevel[1]
+
+  return color
+}
+
+// Subtrack Abbreviations
+export function getSubtrackAbbreviation(subtrack) {
+  const subtrackAbbreviations = {
+    APPLICATION_FRONT_END_DESIGN  : 'A',
+    ARCHITECTURE                  : 'A',
+    ASSEMBLY_COMPETITION          : 'AC',
+    BANNERS_OR_ICONS              : 'BI',
+    BUG_HUNT                      : 'BH',
+    CODE                          : 'C',
+    CONCEPTUALIZATION             : 'C',
+    CONTENT_CREATION              : 'CC',
+    COPILOT_POSTING               : 'CP',
+    DESIGN_FIRST_2_FINISH         : 'DF2F',
+    FIRST_2_FINISH                : 'FF',
+    IDEA_GENERATION               : 'IG',
+    MARATHON_MATCH                : 'MM',
+    OTHER                         : 'O',
+    PRINT_OR_PRESENTATION         : 'PR',
+    SPECIFICATION                 : 'SPC',
+    SRM                           : 'SRM',
+    TEST_SUITES                   : 'TS',
+    UI_PROTOTYPE_COMPETITION      : 'P',
+    WEB_DESIGN                    : 'W',
+    WIDGET_OR_MOBILE_SCREEN_DESIGN: 'WI',
+    WIREFRAMES                    : 'WF'
+  }
+
+  const abbreviation = subtrackAbbreviations[subtrack] || '?'
+
+  return abbreviation
 }

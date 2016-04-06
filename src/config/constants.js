f2c500ebc1df6ff7f7be214f673b9ffb817869f5
Nick Litwin <nlitwin@appirio.com>
2016-04-06 11:09:26 -0700

    Update urls

--- src/config/constants.js
+++ src/config/constants.js
@@ -14,17 +14,14 @@ export const USERNAME_SEARCH_SUCCESS   = 'USERNAME_SEARCH_SUCCESS'
 export const TOP_MEMBER_SEARCH_SUCCESS = 'TOP_MEMBER_SEARCH_SUCCESS'
 
 // Urls
-export const DOMAIN       = process.env.domain
+export const DOMAIN = process.env.domain
 
 // FIXME: Change to process.env.INTERNAL_API after added to webpack
-export const INTERNAL_API = 'https://internal-api.topcoder-dev.com/v3'
+export const INTERNAL_API = `https://internal-api.${DOMAIN}/v3`
 
-export const memberSearchTagUrl = 'https://xsmjngybcg.execute-api.us-east-1.amazonaws.com/dev/v3/tags/'
+export const memberSearchTagUrl = `${INTERNAL_API}/tags/`
 
 export const memberSearchUrl = `${INTERNAL_API}/members/_search/`
 export const challengeSearchUrl = 'https://ol348e2ya5.execute-api.us-east-1.amazonaws.com/dev'
 
 export const leaderboardUrl = `${INTERNAL_API}/leaderboards/`
-
-// Challenge search
-// https://ol348e2ya5.execute-api.us-east-1.amazonaws.com/dev

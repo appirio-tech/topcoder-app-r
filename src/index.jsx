6672cdefdb22eecb055217257bffd015094cb11a
Nick Litwin <nlitwin@appirio.com>
2016-04-27 15:53:48 -0700

    Add top level connector

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,14 @@ import Router         from 'react-router/lib/Router'
 import store  from './config/store'
 import routes from './routes'
 
+import { configureConnector } from 'tc-accounts'
+import { CONNECTOR_URL } from './config/constants'
+
+configureConnector({
+  connectorUrl: CONNECTOR_URL,
+  frameId: 'tc-accounts-iframe'
+})
+
 const mountNode = document.getElementById('root')
 
 render((
